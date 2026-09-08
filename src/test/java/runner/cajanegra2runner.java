9afb8d6f5ea922b79d978a20a878de6b26e6ea1e
Maria Enith Franco <recursosparatesting@gmail.com>
2026-09-07 19:20:04 -0500

    agregar informe de cucumber

--- src/test/java/runner/cajanegra2runner.java
+++ src/test/java/runner/cajanegra2runner.java
@@ -1,10 +1,26 @@
 package runner;
 
-import com.intuit.karate.junit5.Karate;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class cajanegra2runner {
-    @Karate.Test
-    Karate testAll() {
-        return Karate.run("classpath:features").relativeTo(getClass());
+    //@Karate.Test
+    //Karate testAll() {
+    //    return Karate.run("classpath:features").relativeTo(getClass());
+    //}
+    @Test
+    void testParallel() {
+        Results results = Runner.path("classpath:features")
+                .outputCucumberJson(true) // <--- AQUÍ SE CONFIGURA
+                .parallel(1);             // Define el número de hilos concurrentes
+
+        // Aserción para asegurar que el build de Gradle/Maven falle si alguna prueba no pasa
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
 }
+
+
+
